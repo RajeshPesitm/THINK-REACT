1269471d11d93ed346fc2b09b7345a34fddea7d1
Your Name <you@example.com>
2026-04-27 12:54:48 +0530

    Scaffolding Done

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { FilterableProductTable } from './components';
+
 const PRODUCTS = [
   {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
   {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
@@ -9,86 +11,4 @@ const PRODUCTS = [
 
 export default function App() {
   return <FilterableProductTable products={PRODUCTS} />;
-}
-
-function FilterableProductTable({ products }) {
-  return (
-    <div>
-      <SearchBar />
-      <ProductTable products={products} />
-    </div>
-  );
-}
-
-function SearchBar() {
-  return (
-    <form>
-      <input type="text" placeholder="Search..." />
-      <label>
-        <input type="checkbox" />
-        {' '}
-        Only show products in stock
-      </label>
-    </form>
-  );
-}
-
-function ProductTable({ products }) {
-  const rows = [];
-  let lastCategory = null;
-
-  products.forEach((product) => {
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category} />
-      );
-    }
-    lastCategory = product.category;
-
-    rows.push(
-      <ProductRow
-        product={product}
-        key={product.name} />
-    );
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>In Stock</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
-}
-
-function ProductCategoryRow({ category }) {
-  return (
-    <tr>
-      <th colSpan="3">
-        {category}
-      </th>
-    </tr>
-  );
-}
-
-function ProductRow({ product }) {
-  const name = product.stocked ? product.name :
-    <span style={{ color: 'red' }}>
-      {product.name}
-    </span>;
-
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{product.price}</td>
-      <td>{product.stocked ? 'Yes' : 'No'}</td>
-    </tr>
-  );
 }
