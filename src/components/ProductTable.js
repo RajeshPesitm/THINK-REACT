248ff60ae7ccb5f3b0eeefbac4366c347d65e10a
Your Name <you@example.com>
2026-04-27 13:29:28 +0530

    SearchBar and Filtering

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,37 +1,47 @@
 import ProductCategoryRow from './ProductCategoryRow';
 import ProductRow from './ProductRow';
 
-export default function ProductTable({ products }) {
-  const rows = [];
-  let lastCategory = null;
+export default function ProductTable({ products, filterText, inStockOnly }) {
+    const rows = [];
+    let lastCategory = null;
 
-  products.forEach((product) => {
-    if (product.category !== lastCategory) {
-      rows.push(
-        <ProductCategoryRow
-          category={product.category}
-          key={product.category} />
-      );
-    }
-    lastCategory = product.category;
+    products.forEach((product) => {
+        if (
+            product.name.toLowerCase().indexOf(
+                filterText.toLowerCase()
+            ) === -1
+        ) {
+            return;
+        }
+        if (inStockOnly && !product.stocked) {
+            return;
+        }
+        if (product.category !== lastCategory) {
+            rows.push(
+                <ProductCategoryRow
+                    category={product.category}
+                    key={product.category} />
+            );
+        }
+        lastCategory = product.category;
 
-    rows.push(
-      <ProductRow
-        product={product}
-        key={product.name} />
+        rows.push(
+            <ProductRow
+                product={product}
+                key={product.name} />
+        );
+    });
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Price</th>
+                    <th>In Stock</th>
+                </tr>
+            </thead>
+            <tbody>{rows}</tbody>
+        </table>
     );
-  });
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>In Stock</th>
-        </tr>
-      </thead>
-      <tbody>{rows}</tbody>
-    </table>
-  );
 }
